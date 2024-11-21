dbe1ac851416f1738101280318ff31d30e764b75
uetak330 <uetak330@gmail.com>
2024-11-21 23:54:36 +0900

    顧客クラスの作成

---
+++ src/main/java/com/example/domain/model/Customer.java
@@ -0,0 +1,19 @@
+package com.example.domain.model;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+	
+	private int customerId;
+	private String lastName;
+	private String firstName;
+	private String phoneNumber;
+	private String email;
+	private String address;
+	private MembershipLevel membershipLevel;
+	private LocalDateTime createdAt;
+	private LocalDateTime updateAt;
+}
