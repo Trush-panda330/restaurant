207040915a7097ee8a752893ad6aaa85249be024
uetak330 <uetak330@gmail.com>
2024-11-23 03:47:47 +0900

    自動採番の設定をinsertメソッドに設定

--- src/main/java/com/example/domain/customer/mapper/CustomerMapper.java
+++ src/main/java/com/example/domain/customer/mapper/CustomerMapper.java
@@ -3,15 +3,21 @@ package com.example.domain.customer.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 
 import com.example.domain.customer.model.Customer;
 
 @Mapper
 public interface CustomerMapper {
-	
-	List<Customer> findAll();
-	Customer findById(int id);
-	int insert(Customer customer);
-	int update(Customer customer);
-	int deleteById(int id);
+    
+    List<Customer> findAll();
+    
+    Customer findById(int id);
+    
+    @Options(useGeneratedKeys = true, keyProperty = "customerId")
+    int insert(Customer customer);
+    
+    int update(Customer customer);
+    
+    int deleteById(int id);
 }
