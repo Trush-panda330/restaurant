b53717a4f36c6de5677ff511fd0e19a290377422
uetak330 <uetak330@gmail.com>
2024-11-24 23:39:04 +0900

    フィールドの追加
    long型のtimestampを用意しておき
    使う側はSystemクラスのcurrentTimeMillis()を使いlong型の値のtimestampに格納する

--- src/main/java/com/example/domain/customer/response/CustomerErrorResponse.java
+++ src/main/java/com/example/domain/customer/response/CustomerErrorResponse.java
@@ -2,13 +2,17 @@ package com.example.domain.customer.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class CustomerErrorResponse {
-	private int statusCode;    //HTTPステータスコード（例：404）
-	private String error;		   //エラーの種類（例：Not Found）
- 	private String message;  // エラーメッセージ（例：顧客IDが見つかりません）
+	private int statusCode; 				//HTTPステータスコード（例：404）
+	private String error;						//エラーの種類（例：Not Found）
+	private String message; 				// エラーメッセージ（例：顧客IDが見つかりません）
+	private long timestamp; //エラー発生時刻
+
 
 	@Override
 	public String toString() {
@@ -16,7 +20,7 @@ public class CustomerErrorResponse {
 				"statusCode" + statusCode +
 				", error='" + error + '\'' +
 				", message='" + message + '\'' +
+				", timestamp=" + timestamp +
 				'}';
 	}
-
 }
