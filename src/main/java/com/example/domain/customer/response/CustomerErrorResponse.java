1ef3e7d7ac1fbd49f6dd9460fab10adc9149faec
uetak330 <uetak330@gmail.com>
2024-11-25 16:52:18 +0900

    このクラスは一旦使いません
    消してしまってもいいので後々消すかも。
    本当は要らないクラスを置いておくのは良くないのですが一旦保留します

--- src/main/java/com/example/domain/customer/response/CustomerErrorResponse.java
+++ src/main/java/com/example/domain/customer/response/CustomerErrorResponse.java
@@ -1,26 +1,27 @@
-package com.example.domain.customer.response;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-public class CustomerErrorResponse {
-	private int statusCode; 				//HTTPステータスコード（例：404）
-	private String error;						//エラーの種類（例：Not Found）
-	private String message; 				// エラーメッセージ（例：顧客IDが見つかりません）
-	private long timestamp; //エラー発生時刻
-
-
-	@Override
-	public String toString() {
-		return "CustomerErrorResponse{" +
-				"statusCode" + statusCode +
-				", error='" + error + '\'' +
-				", message='" + message + '\'' +
-				", timestamp=" + timestamp +
-				'}';
-	}
-}
+//package com.example.domain.customer.response;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Data
+//public class CustomerErrorResponse {
+//	private int statusCode; 				//HTTPステータスコード（例：404）
+//	private String error;						//エラーの種類（例：Not Found）
+//	private String message; 				// エラーメッセージ（例：顧客IDが見つかりません）
+//	private long timestamp; //エラー発生時刻
+//
+//
+//	@Override
+//	public String toString() {
+//		return "CustomerErrorResponse{" +
+//				"statusCode" + statusCode +
+//				", error='" + error + '\'' +
+//				", message='" + message + '\'' +
+//				", timestamp=" + timestamp +
+//				'}';
+//	}
+//}
+//  commonパッケージにApiErrorResponseを作成しそちらを使ってみます。
